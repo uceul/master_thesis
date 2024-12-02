4b35e928d438c80abddf07a566164328f3b279f6
uceul <uceul@student.kit.edu>
2024-12-02 21:09:03 +0100

    eval scripts

---
+++ legacy_code/scripts/evaluate_llama_2_7b.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+poetry run main evaluate --settings settings/settings_7b.yml --only-model "LLaMa 2 7B"
---
+++ legacy_code/scripts/evaluate_llama_31_8b_instruct.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+poetry run main evaluate --settings settings/settings_llama_31.yml --stats_path llama_31_8b_instruct_stats.yml --only-model "LLaMa 3.1 8B Instruct"
